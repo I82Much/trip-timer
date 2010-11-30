6870cdad70123ebc900150ce7beb7ab8612958b6
Nicholas Dunn <nicholasdunn@Nicholas-Dunns-MacBook-Pro.local>
2010-11-29 23:55:42 -0500

    Fixing a lot of bugs involving the state machine.

--- src/net/developmentality/triptimer/TripTimer.java
+++ src/net/developmentality/triptimer/TripTimer.java
@@ -12,14 +12,28 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class TripTimer extends Activity {
-	private Date appStartTime;
 	private Date startTime;
+	private Date pauseTimeStart;
+	private Date pauseTimeEnd;
 	private Date endTime;
+	
+	private long elapsedPauseTime = 0;
 	
 	private long startUptimeMs;
 	
+	private enum TimerState {
+		NOT_STARTED,
+		RUNNING,
+		PAUSED,
+		STOPPED
+	}
 	
-	private Button startStopButton;
+	private TimerState curState = TimerState.NOT_STARTED;
+	
+	
+	private Button startPauseButton;
+	private Button stopButton;
+	
 	private TextView digitalClock;
 	private Handler mHandler = new Handler();
 	
@@ -28,66 +42,124 @@ public class TripTimer extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        appStartTime = new Date();
         setContentView(R.layout.main);
-        startStopButton = (Button) findViewById(R.id.startStop);
+        startPauseButton = (Button) findViewById(R.id.start);
+        stopButton = (Button) findViewById(R.id.stop);
         digitalClock = (TextView) findViewById(R.id.digitalClock);
     }
     
     // modified from http://developer.android.com/resources/articles/timed-ui-updates.html
     private Runnable mUpdateTimeTask = new Runnable() {
  	   public void run() {
- 		   final long start = startUptimeMs;//startTime.getTime();
- 	       long millis =SystemClock.uptimeMillis() - start;
- 	       int seconds = (int) (millis / 1000);
- 	       int minutes = seconds / 60;
- 	       seconds     = seconds % 60;
- 	       
- 	       if (seconds < 10) {
- 	           digitalClock.setText("" + minutes + ":0" + seconds);
- 	       } else {
- 	    	  digitalClock.setText("" + minutes + ":" + seconds);            
- 	       }
- 	     
+ 		   final long start = startUptimeMs;
+ 		   // Subtract out the paused time
+ 	       long millis =SystemClock.uptimeMillis() - start - elapsedPauseTime;
+ 	       digitalClock.setText(minutesSecondsFormat(millis));
  	       mHandler.postAtTime(this,
- 	               start + (((minutes * 60) + seconds + 1) * 1000));
+ 	    		   SystemClock.uptimeMillis() + 1000);
  	   }
  	};
     
-    // modified from http://developer.android.com/resources/articles/timed-ui-updates.html
+    public void startOrPause(View view) {
+    	switch (curState) {
+	    	case NOT_STARTED:
+	    		startClock();
+	    		break;
+	    	case RUNNING:
+	    		pauseClock();
+	    		break;
+	    	case PAUSED:
+	    		resumeClock();
+	    		break;
+	    	case STOPPED:
+	    		resetClock();
+	    		break;
+    	}
+    }
+    
+ // modified from http://developer.android.com/resources/articles/timed-ui-updates.html
     private void startClock() {
     	startTime = new Date();
 		startUptimeMs = SystemClock.uptimeMillis();
-		startStopButton.setText(R.string.stop);
+		startPauseButton.setText(R.string.pause);
         mHandler.postDelayed(mUpdateTimeTask, 100);
+        stopButton.setEnabled(true);
+        curState = TimerState.RUNNING;
+    }
+    
+    private void pauseClock() {
+    	pauseTimeStart = new Date();
+    	startPauseButton.setText(R.string.resume);
+    	mHandler.removeCallbacks(mUpdateTimeTask);
+    	curState = TimerState.PAUSED;
+    }
+    
+    private void resumeClock() {
+    	pauseTimeEnd = new Date();
+    	elapsedPauseTime += pauseTimeEnd.getTime() - pauseTimeStart.getTime();
+    	startPauseButton.setText(R.string.pause);
+        mHandler.postDelayed(mUpdateTimeTask, 100);
+        stopButton.setEnabled(true);
+        curState = TimerState.RUNNING;
+    }
+    
+    private void resetClock() {
+    	startTime = null;
+		endTime = null;
+		pauseTimeStart = null;
+		pauseTimeEnd = null;
+		
+		startPauseButton.setText(R.string.start);
+		digitalClock.setText(R.string.empty_time);
+		elapsedPauseTime = 0;
+		
+		curState = TimerState.NOT_STARTED;
     }
     
     private void stopClock() {
 		endTime = new Date();
-		int ms = (int) (endTime.getTime() - startTime.getTime());
-		int minutes = ms/(1000 * 60);
-		int seconds = ms/(1000);
 		
-		Log.i(TAG, "Trip started at " + startTime + " ended at " + endTime + " for a total of " + minutes + 
-				" minutes " + seconds + " seconds");
-		startStopButton.setText(R.string.reset);
+		
+		// If we are paused and we stop without resuming first, we never update
+		// the amount of time paused.
+		if (curState == TimerState.PAUSED) {
+			elapsedPauseTime += System.currentTimeMillis() - pauseTimeStart.getTime();
+		}
+		
+		int ms = (int) (endTime.getTime() - startTime.getTime() - elapsedPauseTime);
+		String pausedTime = minutesSecondsFormat(elapsedPauseTime);
+		String netTime = minutesSecondsFormat(ms);
+
+		
+		Log.i(TAG, "Trip started at " + startTime + " ended at " + endTime + " for a net total of " + netTime + " accounting for a total of " + pausedTime + " paused.");
+		startPauseButton.setText(R.string.reset);
 
     	mHandler.removeCallbacks(mUpdateTimeTask);
+    	stopButton.setEnabled(false);
+    	curState = TimerState.STOPPED;
+    }
+    
+    private String minutesSecondsFormat(long ms) {
+    	int seconds = (int) (ms / 1000);
+       int minutes = seconds / 60;
+       seconds     = seconds % 60;
+       
+       if (seconds < 10) {
+    	   return "" + minutes + ":0" + seconds;
+       } else {
+    	   return "" + minutes + ":" + seconds;            
+       }
+    }
+    
+    
+    public void stop(View view) {
+    	stopClock();
     }
     
     public void toggleTime(View view) {
-    	// Haven't started timing yet
-    	if (startTime == null) {
-    		startClock();
-    	}
-    	else if (endTime == null) {
-    		stopClock();
-    	}
-    	// Reset button was pressed.
-    	else {
-    		startTime = null;
-    		endTime = null;
-    		startStopButton.setText(R.string.start);
-    	}
+    	
+    }
+    public void togglePause(View view) {
+    	
     }
 }
